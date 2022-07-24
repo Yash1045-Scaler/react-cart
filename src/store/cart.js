cffafa8b8f2497c3c95f4ebe018c4178676d7706
Yash1045-Scaler <yash.arora@scaler.com>
2022-07-25 00:41:03 +0530

    Store/cart file update

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,17 +1,15 @@
-const ADD_TO_CART = 'cart/ADD_TO_CART';
-const REMOVE_FROM_CART = 'cart/REMOVE_FROM_CART';
-
+const UPDATE_CART= 'cart/UPDATE_CART';
 
 export const addToCart = (cart, product) => {
     return {
-        type: ADD_TO_CART,
+        type: UPDATE_CART,
         payload: [...cart, product],
     }
 }
 
 export const removeFromCart = (cart,item) => {
     return {
-        type: REMOVE_FROM_CART,
+        type: UPDATE_CART,
         payload : cart.filter((product) => product.id !== item.id),
     }
 }
@@ -20,15 +18,10 @@ const cartReducer = (state = {
     cart : [],
 },action) => {
     switch(action.type){
-        case ADD_TO_CART:
+        case UPDATE_CART:
             return{
                 ...state,
                 cart : action.payload,
-            }
-        case REMOVE_FROM_CART:
-            return{
-                ...state,
-                cart: action.payload,
             }
         default:
             return{...state}
